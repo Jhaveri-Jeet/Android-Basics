4d094e3b6480d5d19e3e19798dfb84eb6d67dc6b
Jeet-Jhaveri <jeetwork941@gmail.com>
2023-10-24 13:05:56 +0530

    main file design changed

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -11,41 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
-//
-//        val intentHome = Intent(this, HomeActivity::class.java)
+
         val intentRegister = Intent(this, RegisterActivity::class.java)
         val intentLogin = Intent(this, LoginActivity::class.java)
-//
+
         val btnRegister = findViewById<Button>(R.id.btnRegister)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
-//        val textRegister = findViewById<TextView>(R.id.textRegister)
-//        val txtUsername = findViewById<TextInputEditText>(R.id.txtUsername)
-//        val txtPassword = findViewById<TextInputEditText>(R.id.txtPassword)
-//
+
         btnRegister.setOnClickListener {
-//            val username = txtUsername.text.toString()
-//            val password = txtPassword.text.toString()
-//
-//            if (username == "" || password == ""){
-//                Toast.makeText(this, "Plz Enter The Information", Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//            intentHome.putExtra("username", "Jeet Jhaveri")
-//            intentHome.putExtra("password", "admin")
-//            intentHome.putExtra("age", "21")
-//            intentHome.putExtra("gender", "Male")
-//            startActivity(intentHome)
-//            finish()
             startActivity(intentRegister)
         }
 
         btnLogin.setOnClickListener{
             startActivity(intentLogin)
         }
-//
-//        textRegister.setOnClickListener{
-//            startActivity(intentRegister)
-//            finish()
-//        }
     }
 }
